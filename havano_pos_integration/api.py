c74bed16b638b075f09d9c924ff9d80291ed1551
Chipo Hameja <chipohameja@gmail.com>
2025-01-12 17:30:41 +0100

    API Changes

--- havano_pos_integration/api.py
+++ havano_pos_integration/api.py
@@ -3,24 +3,26 @@ from frappe import _
 
 @frappe.whitelist()
 def test_api(name):
+    # Create a welcome message
     msg = f"Welcome {name}!!!"
+    # Fetch product details
     product_details = frappe.get_all("Item", fields=["name", "item_code", "item_group"])
     
     return msg
 
-
 @frappe.whitelist()
 def create_pos_opening_entry():
     try:
-        
+        # Get form data
         data = frappe.local.form_dict
 
-        
+        # Check for required fields
         required_fields = ["period_start_date", "company", "user", "pos_profile", "balance_details"]
         for field in required_fields:
             if field not in data:
                 frappe.throw(_("Missing required field: {0}").format(field))
 
+        # Create POS Opening Entry document
         pos_opening_entry = frappe.get_doc({
             "doctype": "POS Opening Entry",
             "period_start_date": data.get("period_start_date"),
@@ -30,70 +32,23 @@ def create_pos_opening_entry():
             "balance_details": data.get("balance_details")
         })
 
+        # Insert and submit the document
         pos_opening_entry.insert()
-
         pos_opening_entry.submit()
 
+        # Commit the transaction
         frappe.db.commit()
 
         return pos_opening_entry.as_dict()
 
     except Exception as e:
+        # Log error and return error message
         frappe.log_error(message=str(e), title="Error creating POS Opening Entry")
         return {"error": str(e)}
 
-
-# @frappe.whitelist()
-# def create_pos_closing_entry():
-#     try:
-#         data = frappe.local.form_dict
-
-        
-#         period_start_date = data.get("period_start_date")
-#         period_end_date = data.get("period_end_date")
-#         company = data.get("company")
-#         user = data.get("user")
-#         pos_profile = data.get("pos_profile")
-#         pos_opening_entry = data.get("pos_opening_entry")
-
-        
-#         pos_opening_doc = frappe.get_doc("POS Opening Entry", pos_opening_entry)
-#         # if not pos_opening_doc.docstatus == 1:
-#         #     frappe.throw(_("POS Opening entry must be submitted and have status as 'OPEN'"))
-
-        
-#         # doc = frappe.get_doc({
-#         #     "doctype": "POS Closing",
-#         #     "period_start_date": period_start_date,
-#         #     "period_end_date": period_end_date,
-#         #     "company": company,
-#         #     "user": user,
-#         #     "pos_profile": pos_profile,
-#         #     "pos_opening_entry": pos_opening_entry
-#         # })
-
-        
-#         # doc.insert()
-
-        
-#         # frappe.db.commit()
-
-#         # Return a success response
-#         # return {"message": _("POS Closing entry created successfully."), "docname": doc.name}
-#         return pos_opening_doc
-
-    except frappe.DoesNotExistError:
-        return {"error": _("The specified POS Opening entry does not exist.")}
-
-    except frappe.ValidationError as e:
-        return {"error": str(e)}
-
-    except Exception as e:
-        return {"error": _("An unexpected error occurred: ") + str(e)}
-
-
 @frappe.whitelist()
 def get_inventory():
+    # Fetch price list, inventory, and item price list
     price_list = frappe.get_all("Price List", fields = ["price_list_name","currency"])
     inventory = frappe.get_all("Bin", fields = ["item_code","valuation_rate","warehouse","actual_qty","ordered_qty","stock_value"])
     item_price_list = frappe.get_all("Item Price", fields = ["item_code","uom","price_list","price_list_rate","currency","currency","supplier"])
@@ -101,8 +56,11 @@ def get_inventory():
 
 @frappe.whitelist()
 def get_warehouses():
+    # Fetch inventory and warehouse details
     inventory = frappe.get_all("Bin", fields=["item_code", "valuation_rate", "warehouse", "actual_qty", "ordered_qty", "stock_value"])
     warehouses = frappe.get_all("Warehouse", fields=["name", "company", "account", "warehouse_type"])
+    
+    # Calculate total quantity and value for each warehouse
     warehouse_inventory = {}
     for item in inventory:
         warehouse = item["warehouse"]
@@ -114,7 +72,7 @@ def get_warehouses():
         warehouse_inventory[warehouse]["total_quantity"] += item["actual_qty"]
         warehouse_inventory[warehouse]["total_value"] += item["stock_value"]
 
-    
+    # Add total quantity and value to warehouse details
     for warehouse in warehouses:
         name = warehouse["name"]
         warehouse["total_quantity"] = warehouse_inventory.get(name, {}).get("total_quantity", 0)
@@ -122,18 +80,14 @@ def get_warehouses():
 
     return warehouses
 
-
 @frappe.whitelist()
 def get_cost_center():
+    # Fetch cost center details
     return frappe.get_all("Cost Center", fields = ["cost_center_name", "cost_center_number", "parent_cost_center", "company"])
-
-# @frappe.whitelist()
-# def get_pos_profile():
-#     return frappe.get_all("POS Profile", fields = ["name","company", "warehouse", "customer", "company_address","cost_center","applicable_for_users","payments"])
 
 @frappe.whitelist()
 def get_pos_profile():
-    
+    # Fetch POS profile details
     pos_profiles = frappe.get_all("POS Profile", fields=["name", "company", "warehouse", "customer", "company_address", "cost_center","selling_price_list"])
 
     response = []
@@ -151,6 +105,7 @@ def get_pos_profile():
             "price_list": profile.selling_price_list
         }
 
+        # Fetch applicable users for the profile
         try: 
             if frappe.db.exists("POS Profile User",{"parent":profile.name}):
                 applicable_for_users = frappe.get_all("POS Profile User",filters={"parent": profile.name}, fields=["user","default"])
@@ -160,6 +115,7 @@ def get_pos_profile():
         except Exception as e: 
             profile_data["applicable_for_users"] = []
         
+        # Fetch payment methods for the profile
         try: 
             if frappe.db.exists("POS Payment Method",{"parent":profile.name}):
                payments = frappe.get_all("POS Payment Method", filters={"parent": profile.name}, fields=["mode_of_payment", "default"])
@@ -172,12 +128,6 @@ def get_pos_profile():
         response.append(profile_data)
 
     return response
-
-# @frappe.whitelist()
-# def get_products():
-#     prices = frappe.get_all("Price List", fields = ["price_list_name","currency"])
-#     products = frappe.get_all("Bin", fields = ["item_code", "actual_qty", "warehouse"])
-#     return { "products": products , "price_list": prices }
 
 @frappe.whitelist()
 def get_products():
@@ -239,34 +189,59 @@ def get_products():
 
     return {"products": final_products}
 
-
 @frappe.whitelist()
 def get_sales_invoice():
-    return frappe.get_all("Sales Invoice", fields = ["customer","company", "customer_name", "posting_date", "posting_time","due_date"])
+    # Fetch sales invoice details
+    final_invoice = []
+    sales_invoice_list =  frappe.get_all("Sales Invoice", fields = ["name", "customer","company", "customer_name", "posting_date", "posting_time","due_date","total_qty","total","total_taxes_and_charges","grand_total"])
+    for invoice in sales_invoice_list:
+        # Fetch items for each invoice
+        items = frappe.get_all("Sales Invoice Item", filters={"parent": invoice.name},fields = ["item_name", "qty", "rate", "amount",])
+        invoice = {
+            "name": invoice.name,
+            "customer": invoice.customer,
+            "company": invoice.company,
+            "customer_name": invoice.customer_name,
+            "posting_date": invoice.posting_date,
+            "posting_time": invoice.posting_time,
+            "due_date": invoice.due_date,
+            "items": items,
+            "total_qty": invoice.total_qty,
+            "total": invoice.total,
+            "total_taxes_and_charges": invoice.total_taxes_and_charges,
+            "grand_total": invoice.grand_total
+        }
+        final_invoice.append(invoice)
+    
+    return final_invoice
 
 @frappe.whitelist()
 def get_user():
+    # Fetch user details
     return frappe.get_all("User", fields = ["email","first_name", "last_name", "username", "gender","location"])
 
 @frappe.whitelist()
 def get_customer():
+    # Fetch customer details with default price list
     customers = frappe.get_all("Customer", filters = {"default_price_list": ["!=", ""]} ,fields = ["customer_name","customer_type","gender","customer_pos_id","default_price_list"])
-
     for customer in customers:
+        # Fetch item prices for each customer
         customer.items = frappe.get_all("Item Price", filters = {"price_list":customer.default_price_list}, fields = ["item_code","item_name","price_list_rate"])
     return customers
 
 @frappe.whitelist()
 def get_account():
+    # Fetch account details
     return frappe.get_all("Account", fields = ["account_name","account_number","company","parent_account","account_type"])
 
 def submit_pos_opening_entry(doc,method):
-    #frappe.msgprint("POS Opening Entry Submitted!")
-    #doc = frappe.get_doc('POS Opening Entry', doc.name)
+    # Submit POS Opening Entry document
     doc.submit()
 
 def submit_pos_closing_entry(doc, method=None):
+    # Submit POS Closing Entry document
     doc.submit()
 
 def submit_pos_invoice(doc, method=None):
+    # Submit POS Invoice document
     doc.submit()
