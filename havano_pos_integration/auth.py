7bb3f097ec1f1bfb3d267c134bfd873cafc222e3
Nasirucode <akingbolahan12@gmail.com>
2025-02-14 12:28:08 +0100

    Feature: Login Added

---
+++ havano_pos_integration/auth.py
@@ -0,0 +1,90 @@
+import frappe
+from frappe.utils import escape_html,cstr
+from frappe.auth import LoginManager
+from frappe import throw, msgprint, _
+from frappe.utils.background_jobs import enqueue
+import requests
+import random
+import json
+import base64
+from ess_backend_api.ess_backend_api.apis.utils import create_response
+
+
+
+@frappe.whitelist(allow_guest=True)
+def login(usr,pwd):
+    try:
+        login_manager = frappe.auth.LoginManager()
+        login_manager.authenticate(user=usr,pwd=pwd)
+        login_manager.post_login()
+    except frappe.exceptions.AuthenticationError:
+        frappe.clear_messages()
+        frappe.local.response.http_status_code = 422
+        frappe.local.response["message"] =  "Invalid Email or Password"
+        return
+    
+    user = frappe.get_doc('User',frappe.session.user)
+
+    api_generate=generate_keys(user)
+       
+    token_string = str(api_generate['api_key']) +":"+ str(api_generate['api_secret'])
+
+    default_company = frappe.db.get_single_value('Global Defaults','default_company')
+    if default_company:
+        default_company_doc = frappe.get_doc("Company" , default_company) 
+
+    frappe.response["user"] =   {
+        "first_name": escape_html(user.first_name or ""),
+        "last_name": escape_html(user.last_name or ""),
+        "gender": escape_html(user.gender or "") or "",
+        "birth_date": user.birth_date or "",       
+        "mobile_no": user.mobile_no or "",
+        "username":user.username or "",
+        "full_name":user.full_name or "",
+        "email":user.email or "",
+        "company" : {
+            "name" : default_company_doc.name or "",
+            "email" : default_company_doc.email or "",
+            "website" : default_company_doc.website or ""
+        }
+    }
+    frappe.response["token"] =  base64.b64encode(token_string.encode("ascii")).decode("utf-8")
+
+    return
+
+
+def generate_keys(user):
+    api_secret = api_key = ''
+    if not user.api_key and not user.api_secret:
+        api_secret = frappe.generate_hash(length=15)
+        # if api key is not set generate api key
+        api_key = frappe.generate_hash(length=15)
+        user.api_key = api_key
+        user.api_secret = api_secret
+        user.save(ignore_permissions=True)
+    else:
+        api_secret = user.get_password('api_secret')
+        api_key = user.get('api_key')
+    return {"api_secret": api_secret, "api_key": api_key}
+
+# For Verfiy OTP Function
+@frappe.whitelist(allow_guest=True)
+def logout(user):
+    try:
+        user = frappe.get_doc("User",user)
+        user.api_key = None
+        user.api_secret = None
+        user.save(ignore_permissions = True)
+        
+        frappe.local.login_manager.logout()
+        create_response(200, "Logged Out Successfully")
+        return
+    except frappe.DoesNotExistError:
+        # Handle case where user document is not found
+        frappe.log_error(f"User '{user}' does not exist.", "Logout Failed")
+        create_response(404, "User not found")
+        return
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "Logout Failed")
+        create_response(417, "Something went wrong", str(e))
+        return
