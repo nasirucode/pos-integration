4cec64c4939a7eafb4969612530f0449a580c6fa
Nasirucode <akingbolahan12@gmail.com>
2025-03-09 15:07:59 +0100

    Fix: filters added to payroll

--- havano_pos_integration/havano_pos_integration/report/nssa_report/nssa_report.js
+++ havano_pos_integration/havano_pos_integration/report/nssa_report/nssa_report.js
@@ -3,6 +3,23 @@
 
 frappe.query_reports["NSSA Report"] = {
 	"filters": [
-
+		{
+			fieldname: "currency",
+			label: __("Currency"),
+			fieldtype: "Link",
+			options: "currency",
+			reqd: 1,
+		},
+		{
+			fieldname: "payroll_frequency",
+			fieldtype: "Date",
+			label: __("Payroll Frequency"),
+		},
+		{
+			fieldname: "employee",
+			fieldtype: "Link",
+			label: __("Employee"),
+			options: "Employee",
+		},
 	]
 };
