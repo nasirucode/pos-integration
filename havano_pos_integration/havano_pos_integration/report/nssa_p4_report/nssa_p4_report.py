82ed338b2598349e94ce6648586b312bf41fcfb6
Nasirucode <akingbolahan12@gmail.com>
2025-03-10 20:22:35 +0100

    Fix: zimra p2f form

--- havano_pos_integration/havano_pos_integration/report/nssa_p4_report/nssa_p4_report.py
+++ havano_pos_integration/havano_pos_integration/report/nssa_p4_report/nssa_p4_report.py
@@ -1,9 +1,74 @@
 # Copyright (c) 2025, Alphazen Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
-
+import frappe
+from frappe.utils import flt
 
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+    columns = get_columns(filters)
+    data = get_data(filters)
+    return columns, data
+
+def get_columns(filters):
+    currency_label = filters.get("currency", "Currency")
+    
+    columns = [
+        {"label": "Surname", "fieldname": "surname", "fieldtype": "Data", "width": 150},
+        {"label": "First Names", "fieldname": "first_names", "fieldtype": "Data", "width": 200},
+        {"label": "Start Date", "fieldname": "start_date", "fieldtype": "Date", "width": 150},
+        {"label": "End Date", "fieldname": "end_date", "fieldtype": "Date", "width": 150},
+    ]
+    
+    if currency_label == "USD":
+        columns.extend([
+            {"label": "Total Insurable Earnings (USD)", "fieldname": "total_insurable_earnings_usd", "fieldtype": "Currency", "width": 180},
+            {"label": "Current Contributions (USD)", "fieldname": "current_contributions_usd", "fieldtype": "Currency", "width": 180},
+            {"label": "Arrears (USD)", "fieldname": "arrears_usd", "fieldtype": "Currency", "width": 150},
+            {"label": "Prepayments (USD)", "fieldname": "prepayments_usd", "fieldtype": "Currency", "width": 150},
+            {"label": "Surcharge (USD)", "fieldname": "surcharge_usd", "fieldtype": "Currency", "width": 150},
+            {"label": "Total Payment (USD)", "fieldname": "total_payment_usd", "fieldtype": "Currency", "width": 180}
+        ])
+    elif currency_label == "ZWL":
+        columns.extend([
+            {"label": "Total Insurable Earnings (ZWL)", "fieldname": "total_insurable_earnings_zwl", "fieldtype": "Currency", "width": 180},
+            {"label": "Current Contributions (ZWL)", "fieldname": "current_contributions_zwl", "fieldtype": "Currency", "width": 180},
+            {"label": "Arrears (ZWL)", "fieldname": "arrears_zwl", "fieldtype": "Currency", "width": 150},
+            {"label": "Prepayments (ZWL)", "fieldname": "prepayments_zwl", "fieldtype": "Currency", "width": 150},
+            {"label": "Surcharge (ZWL)", "fieldname": "surcharge_zwl", "fieldtype": "Currency", "width": 150},
+            {"label": "Total Payment (ZWL)", "fieldname": "total_payment_zwl", "fieldtype": "Currency", "width": 180}
+        ])
+    
+    return columns
+
+def get_data(filters):
+    conditions = ""
+    if filters.get("currency"):
+        conditions += f" AND ss.currency = '{filters.get('currency')}'"
+    if filters.get("payroll_period"):
+        conditions += f" AND CONCAT(ss.start_date, ' to ', ss.end_date) = '{filters.get('payroll_period')}'"
+    
+    query = f"""
+        SELECT
+            emp.last_name AS surname,
+            emp.first_name AS first_names,
+            ss.start_date,
+            ss.end_date,
+            CASE WHEN ss.currency = 'USD' THEN ss.gross_pay END AS total_insurable_earnings_usd,
+            CASE WHEN ss.currency = 'USD' THEN (SELECT amount FROM `tabSalary Detail` WHERE parent=ss.name AND salary_component='NSSA Contribution') END AS current_contributions_usd,
+            CASE WHEN ss.currency = 'USD' THEN (SELECT amount FROM `tabSalary Detail` WHERE parent=ss.name AND salary_component='NSSA Arrears') END AS arrears_usd,
+            CASE WHEN ss.currency = 'USD' THEN (SELECT amount FROM `tabSalary Detail` WHERE parent=ss.name AND salary_component='NSSA Prepayments') END AS prepayments_usd,
+            CASE WHEN ss.currency = 'USD' THEN (SELECT amount FROM `tabSalary Detail` WHERE parent=ss.name AND salary_component='NSSA Surcharge') END AS surcharge_usd,
+            CASE WHEN ss.currency = 'USD' THEN (SELECT amount FROM `tabSalary Detail` WHERE parent=ss.name AND salary_component='NSSA Total Payment') END AS total_payment_usd,
+            CASE WHEN ss.currency = 'ZWL' THEN ss.gross_pay END AS total_insurable_earnings_zwl,
+            CASE WHEN ss.currency = 'ZWL' THEN (SELECT amount FROM `tabSalary Detail` WHERE parent=ss.name AND salary_component='NSSA Contribution') END AS current_contributions_zwl,
+            CASE WHEN ss.currency = 'ZWL' THEN (SELECT amount FROM `tabSalary Detail` WHERE parent=ss.name AND salary_component='NSSA Arrears') END AS arrears_zwl,
+            CASE WHEN ss.currency = 'ZWL' THEN (SELECT amount FROM `tabSalary Detail` WHERE parent=ss.name AND salary_component='NSSA Prepayments') END AS prepayments_zwl,
+            CASE WHEN ss.currency = 'ZWL' THEN (SELECT amount FROM `tabSalary Detail` WHERE parent=ss.name AND salary_component='NSSA Surcharge') END AS surcharge_zwl,
+            CASE WHEN ss.currency = 'ZWL' THEN (SELECT amount FROM `tabSalary Detail` WHERE parent=ss.name AND salary_component='NSSA Total Payment') END AS total_payment_zwl
+        FROM `tabSalary Slip` ss
+        JOIN `tabEmployee` emp ON ss.employee = emp.name
+        WHERE ss.docstatus = 1 {conditions}
+    """
+    
+    return frappe.db.sql(query, as_dict=True)
+
