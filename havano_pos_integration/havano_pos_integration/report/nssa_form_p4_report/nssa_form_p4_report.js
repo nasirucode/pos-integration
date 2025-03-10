642cc1782677a5eacc3c561785d402bb84cb0bc8
Nasirucode <akingbolahan12@gmail.com>
2025-03-10 21:28:08 +0200

    nssa form p4 added

---
+++ havano_pos_integration/havano_pos_integration/report/nssa_form_p4_report/nssa_form_p4_report.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Alphazen Technologies and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["NSSA Form P4 Report"] = {
+	"filters": [
+
+	]
+};
