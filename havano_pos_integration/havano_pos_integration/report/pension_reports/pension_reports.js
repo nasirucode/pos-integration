737a1095c7dc2afa77744904b6050bf0a5ab81b0
Nasirucode <akingbolahan12@gmail.com>
2025-07-27 19:41:33 +0100

    Fix: Pension Report Added

--- havano_pos_integration/havano_pos_integration/report/pension_reports/pension_reports.js
+++ havano_pos_integration/havano_pos_integration/report/pension_reports/pension_reports.js
@@ -3,6 +3,27 @@
 
 frappe.query_reports["PENSION Reports"] = {
 	"filters": [
-
-	]
+        {
+            "fieldname": "company",
+            "label": __("Company"),
+            "fieldtype": "Link",
+            "options": "Company",
+            "reqd": 1,
+            "default": frappe.defaults.get_user_default("Company")
+        },
+        {
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "reqd": 1,
+            "default": frappe.datetime.add_months(frappe.datetime.get_today(), -1) // last month
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "reqd": 1,
+            "default": frappe.datetime.get_today() // today's date
+        }
+    ]
 };
